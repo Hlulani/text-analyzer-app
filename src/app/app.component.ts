7b8411be87c9d4621abff3d7cf0b57b3fac6d4ed
Hlulani <hlullyr@gmail.com>
2022-01-17 23:46:17 +0100

    Added reused code for counting vowels and consonants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'text-analyzer-app';
+  vowels: any = [];
+  vowelsCountMap: any = {};
+  consonants: any = [];
+  consonantsCountMap: any = {};
+  displayVal = '';
+  getValuesToAnalyze1(value: string) {
+    console.warn(value);
+    this.displayVal = value;
+  }
+
+  getValuesToAnalyze(type: string, chars: string) {
+    console.log('called');
+    this.vowels = [];
+    this.vowelsCountMap = {};
+    this.consonants = [];
+    this.consonantsCountMap = {};
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    if (type == 'vowels') {
+      for (let i = 0; i < chars.length; i++) {
+        const char = chars[i].toLowerCase();
+
+        if (vowels.indexOf(char) != -1) {
+          this.vowelsCountMap[char] = this.vowelsCountMap[char]
+            ? this.vowelsCountMap[char] + 1
+            : 1;
+        }
+      }
+      this.vowels = Object.keys(this.vowelsCountMap);
+      console.log(this.vowels);
+    } else if (type == 'consonants') {
+      for (let i = 0; i < chars.length; i++) {
+        const char = chars[i].toLowerCase();
+
+        if (vowels.indexOf(char) == -1 && char != ' ') {
+          this.consonantsCountMap[char] = this.consonantsCountMap[char]
+            ? this.consonantsCountMap[char] + 1
+            : 1;
+        }
+      }
+      this.consonants = Object.keys(this.consonantsCountMap);
+    }
+  }
 }
